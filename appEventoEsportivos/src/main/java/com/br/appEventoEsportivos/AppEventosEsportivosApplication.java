5667c37d57956fda0f3d0a7c2a1a19a38377050b
douglassutil <douglassutil@gmail.com>
2019-05-01 17:26:08 -0300

    correção nome

--- appEventoEsportivos/src/main/java/com/br/appEventoEsportivos/AppEventosEsportivosApplication.java
+++ appEventoEsportivos/src/main/java/com/br/appEventoEsportivos/AppEventosEsportivosApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestePicPayApplication {
+public class AppEventosEsportivosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestePicPayApplication.class, args);
+		SpringApplication.run(AppEventosEsportivosApplication.class, args);
 	}
 
 }
--- appEventoEsportivos/src/test/java/com/br/appEventosEsportivos/AppEventosEsportivosApplicationTests.java
+++ appEventoEsportivos/src/test/java/com/br/appEventosEsportivos/AppEventosEsportivosApplicationTests.java
@@ -1,4 +1,4 @@
-package com.br.testePicPay;
+package com.br.appEventosEsportivos;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TestePicPayApplicationTests {
+public class AppEventosEsportivosApplicationTests {
 
 	@Test
 	public void contextLoads() {
